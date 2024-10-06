6a7828cde1777af1588e777cdfcc2eb33f5bcbf2
khaled07-ag <khaled-kk01@hotmail.com>
2024-10-06 17:36:24 +0300

    navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -55,7 +55,7 @@ const Navbar = () => {
                     Recipes
                   </NavLink>
                   <NavLink
-                    to="/"
+                    to="/login"
                     className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
                     Login
